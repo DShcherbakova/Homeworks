8c0e29b797f764ae6bad4a43caa89b28b2ec8924
Daria Shcherbakova <Daria>
2024-03-06 20:10:10 +0100

    init

--- src/l33/Main.java
+++ src/l33/Main.java
@@ -1,2 +1,15 @@
-package l33;public class Main {
+package l33;
+
+public class Main {
+    public static void main(String[] args) {
+        myBox<Integer> box1 = new myBox<>(); // Создание двух Box объектов для Integer
+        box1.set(134);                       // Установка значения для первого Box
+
+        myBox<Integer> box2 = new myBox<>();
+        box2.set(134);
+
+        boolean isEqual;
+        isEqual = myBox.isEqual(box1, box2);
+        System.out.println("Ответ: " + isEqual);
+    }
 }
--- src/l33/myBox.java
+++ src/l33/myBox.java
@@ -1,2 +1,23 @@
-package l33;public class myBox {
+package l33;
+
+
+class myBox<T> {     // Объявление обобщенного класса myBox с типом-параметром T
+    private T data;    // Поле "data" типа T
+    public T get() {        // Метод для получения значения поля "content"
+
+        return data;
+    }
+    public void set(T data) {    // Метод для установки значения поля "content"
+
+        this.data = data;
+    }
+    public static <T> boolean isEqual(myBox<T> box1, myBox<T> box2) {
+
+        return box1.get().equals(box2.get());           // Сравнение содержимого двух Box объектов
+    }
+    static boolean checkBox(myBox<String> stringBox) {
+
+        return stringBox.get() != null;
+    }
 }
+
